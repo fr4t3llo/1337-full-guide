e557d364bc39505ea8efa9177fad94b441332df6
saifeddinekasmi <saife.addine123@gmail.com>
2023-09-27 03:57:43 +0100

    FRATELLO

---
+++ C Piscine C 03/ex04/ft_strstr.c
@@ -0,0 +1,23 @@
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	if (to_find[0] == '\0')
+		return (str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while ((to_find[j] != '\0') && (str[i + j] == to_find[j]))
+		{
+			j++;
+		}
+		if (to_find[j] == '\0')
+		{
+			return (str + i);
+		}
+		i++;
+	}
+	return (0);
+}
